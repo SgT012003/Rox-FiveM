816dea230d5821a415af9354ac8417c76e9bd648
SgT <82065998+SgT012003@users.noreply.github.com>
2022-10-13 16:30:07 -0300

    Add files via upload

---
+++ vrp_kit/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sg-T-(01)#0001'
+description 'Kits'
+version '1.1.3'
+
+client_script {
+    "@vrp/lib/utils.lua",
+    "client/*"
+}
+
+server_script {
+    "@vrp/lib/utils.lua",
+    "server/server.lua"
+}
